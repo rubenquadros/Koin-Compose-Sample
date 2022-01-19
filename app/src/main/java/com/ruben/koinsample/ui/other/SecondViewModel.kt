5436795785044d9f09cddf4b0d3d72ded1d23dde
rubenquadros <rquadros95@gmail.com>
2022-01-19 23:10:24 +0530

    use constant key

--- app/src/main/java/com/ruben/koinsample/ui/other/SecondViewModel.kt
+++ app/src/main/java/com/ruben/koinsample/ui/other/SecondViewModel.kt
@@ -2,6 +2,7 @@ package com.ruben.koinsample.ui.other
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.ruben.koinsample.Destinations.PARAM
 
 /**
  * Created by Ruben Quadros on 19/01/22
@@ -9,7 +10,7 @@ import androidx.lifecycle.ViewModel
 class SecondViewModel(private val handle: SavedStateHandle): ViewModel() {
 
     fun getValue() : String {
-        return handle.get<String>("param") ?: "Y null?"
+        return handle.get<String>(PARAM) ?: "Y null?"
     }
 
 
